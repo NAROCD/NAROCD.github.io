01b1b585e0c632f99a2c276ea0c821f643d05bd2
NAROCD <daniel.narovec@gmail.com>
2024-01-30 16:00:46 +0100

    Update welcome message and copyright year

--- script.js
+++ script.js
@@ -6,16 +6,24 @@ function toggleMenu() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Show popup when page loads
   var popup = document.getElementById("popupOverlay");
   popup.style.display = "block";
-  // Prevent scrolling
   document.body.classList.add("no-scroll");
 });
 
+function updateCopyrightYear() {
+  const currentYear = new Date().getFullYear();
+  const copyrightElement = document.getElementById("copyright");
+  if (copyrightElement) {
+    copyrightElement.textContent = currentYear;
+  }
+}
+
+window.addEventListener("load", function () {
+  updateCopyrightYear();
+});
+
 function closePopup() {
-  // Hide popup
   document.getElementById("popupOverlay").style.display = "none";
-  // Enable scrolling
   document.body.classList.remove("no-scroll");
 }
